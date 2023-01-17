c0a5dc2cd45db62f9b16f3880a622e363a20f370
savindu29 <1savindupanagoda@gmail.com>
2023-01-17 05:49:03 +0530

    completed home

---
+++ js/script.js
@@ -0,0 +1,8 @@
+new Typed('#multi-text',{
+    strings : ['&lt;/>',"&lt;/> Developer ","&lt;/> Undergraduate "],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    delaySpeed : 1500,
+
+});
